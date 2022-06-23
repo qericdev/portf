ec9d980c104074afb3c6981902884d697ec62f72
qericdev <101020988+qericdev@users.noreply.github.com>
2022-06-23 12:46:54 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+  env: {
+    SERVICE_ID : 'service_29d6ifi',
+    TEMPLATE_ID : 'template_u2ocjs7',
+    PUBLIC_KEY : 'eqsrfd7UYvF53NnUz',
+  },
+}
